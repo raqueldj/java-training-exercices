f8a4b2bbaababfd35c234a0c75f007ea98b4f637
raqueldj <raquel.diasjales@gmail.com>
2017-02-28 11:51:08 +0100

    Straight

--- hands/src/main/java/io/robusta/hand/solution/Hand.java
+++ hands/src/main/java/io/robusta/hand/solution/Hand.java
@@ -16,19 +16,17 @@ import io.robusta.hand.interfaces.IDeck;
 import io.robusta.hand.interfaces.IHand;
 import io.robusta.hand.interfaces.IHandResolver;
 
-public class Hand extends TreeSet<Card> implements IHand{
+public class Hand extends TreeSet<Card> implements IHand {
 
-	
 	private static final long serialVersionUID = 7824823998655881611L;
 
 	@Override
 	public Set<Card> changeCards(IDeck deck, Set<Card> cards) {
 		// For exemple remove three cards from this hand
-        // , and get 3 new ones from the Deck
-        // returns the new given cards
+		// , and get 3 new ones from the Deck
+		// returns the new given cards
 		return null;
 	}
-
 
 	@Override
 	public boolean beats(IHand villain) {
@@ -42,14 +40,20 @@ public class Hand extends TreeSet<Card> implements IHand{
 
 	@Override
 	public HandClassifier getClassifier() {
-		
+
 		return this.getValue().getClassifier();
 	}
 
 	@Override
 	public boolean isStraight() {
-
-		return false;
+		
+		List<Card> cards = new ArrayList<>(this);
+		for(int i = 0; i<4; i++){
+			if(cards.get(i+1).getValue() != cards.get(i).getValue()+1){
+				return false;
+			}
+		}
+		return true;
 	}
 
 	@Override
@@ -58,79 +62,83 @@ public class Hand extends TreeSet<Card> implements IHand{
 		return true;
 	}
 
-
-    /**
-     * Returns number of identical cards
-     * 5s5cAd2s3s  has two cardValue of 5
-     */
+	/**
+	 * Returns number of identical cards 5s5cAd2s3s has two cardValue of 5
+	 */
 	@Override
 	public int number(int cardValue) {
 		int result = 0;
-		for (Card current : this){
-			if (current.getValue() == cardValue){
+		for (Card current : this) {
+			if (current.getValue() == cardValue) {
 				result++;
 			}
 		}
 		return result;
 	}
 
-
-    /**
-     * The fundamental map
-     * Check the tests and README to understand
-     */
+	/**
+	 * The fundamental map Check the tests and README to understand
+	 */
 	@Override
 	public HashMap<Integer, List<Card>> group() {
-		HashMap<Integer, List<Card> > map = new HashMap<>();
+		HashMap<Integer, List<Card>> map = new HashMap<>();
 
-        // fill the map
+		ArrayList<Card> list = null;
+		
+		for (Card c : this){
+			// fill the map
+			if (map.get(c.getValue()) == null) {
+				list = new ArrayList<Card>();
+				list.add(c);
+				map.put(c.getValue(), list);
+			}else{
+				list.add(c);
+				map.put(c.getValue(), list);
+			}
 
+		}
+		
 		return map;
 	}
 
-
-    // different states of the hand
+	// different states of the hand
 	int mainValue;
 	int tripsValue;
 	int pairValue;
 	int secondValue;
 	TreeSet<Card> remainings;
 
+	/**
+	 * return all single cards not used to build the classifier
+	 * 
+	 * @param map
+	 * @return
+	 */
+	TreeSet<Card> getGroupRemainingsCard(Map<Integer, List<Card>> map) {
+		TreeSet<Card> groupRemaining = new TreeSet<>();
+		// May be adapted at the end of project:
+		// if straight or flush : return empty
+		// If High card, return 4 cards
 
-    /**
-     * return all single cards not used to build the classifier
-     * @param map
-     * @return
-     */
-    TreeSet<Card>getGroupRemainingsCard(Map<Integer, List<Card>> map){
-        TreeSet<Card> groupRemaining = new TreeSet<>();
-        // May be adapted at the end of project:
-        // if straight or flush : return empty
-        // If High card, return 4 cards
-
-        for (List<Card> group :map.values()){
-            if (group.size() ==1){
-                groupRemaining.add(group.get(0));
-            }
-        }
-        return groupRemaining;
-    }
-
+		for (List<Card> group : map.values()) {
+			if (group.size() == 1) {
+				groupRemaining.add(group.get(0));
+			}
+		}
+		return groupRemaining;
+	}
 
 	@Override
 	public boolean isPair() {
 
-			return false;	
+		return false;
 
-		
 	}
-
 
 	@Override
 	public boolean isDoublePair() {
 		return false;
 	}
-
 
 	@Override
 	public boolean isHighCard() {
@@ -138,44 +146,35 @@ public class Hand extends TreeSet<Card> implements IHand{
 		return true;
 	}
 
-
 	@Override
 	public boolean isTrips() {
 
 		return false;
 	}
 
-
-
 	@Override
 	public boolean isFourOfAKind() {
 
-			return false;	
+		return false;
 
-		
-		
 	}
 
-
-	
 	@Override
 	public boolean isFull() {
 		return false;
 	}
-
 
 	@Override
 	public boolean isStraightFlush() {
 		return false;
 	}
 
-
 	@Override
 	public HandValue getValue() {
 		HandValue handValue = new HandValue();
 
 		// Exemple for FourOfAKind ; // do for all classifiers
-		if(this.isFourOfAKind()){
+		if (this.isFourOfAKind()) {
 			handValue.setClassifier(HandClassifier.FOUR_OF_A_KIND);
 			handValue.setLevelValue(this.mainValue);
 			handValue.setOtherCards(this.remainings); // or this.getRemainings()
@@ -185,31 +184,26 @@ public class Hand extends TreeSet<Card> implements IHand{
 		return handValue;
 	}
 
-
 	@Override
 	public boolean hasCardValue(int level) {
 
 		return false;
 	}
 
-
 	@Override
 	public boolean hasAce() {
 		return false;
 	}
 
-
 	@Override
 	public int highestValue() {
-        // ace might be the highest value
+		// ace might be the highest value
 		return 0;
 	}
-
 
 	@Override
 	public int compareTo(IHandResolver o) {
 		return 0;
 	}
 
-	
 }
